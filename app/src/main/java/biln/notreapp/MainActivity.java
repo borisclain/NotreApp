6408b34958151ffb34c457ac540cc0db2ef2bb78
Boris Clain <sirobclain@gmail.com>
2015-03-13 20:47:58 -0400

    Initial commit

---
+++ app/src/main/java/biln/notreapp/MainActivity.java
@@ -0,0 +1,86 @@
+package biln.notreapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+// commentaire sur l'activité
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    Button buttonSearch;
+    Button buttonConsult;
+
+
+
+    String title;
+    String type;
+    String date;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonSearch = (Button)findViewById(R.id.button1);
+        buttonConsult = (Button)findViewById(R.id.button2);
+
+        buttonSearch.setOnClickListener(this);
+        buttonConsult.setOnClickListener(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.button1 :
+
+                Toast.makeText(this, "Clic bien reçu!", Toast.LENGTH_LONG).show();
+                Intent i = new Intent(this, SecondActivity.class);
+                startActivity(i);
+                break;
+
+            case R.id.button2 :
+                //TODO Implanter la consultation des événements sauvegardés
+                Toast.makeText(this, "Consultation des événements sauvegardés pas encore disponible", Toast.LENGTH_LONG).show();
+                break;
+            default:
+                break;
+        }
+    }
+
+
+
+
+
+
+
+
+
+}
+
+
